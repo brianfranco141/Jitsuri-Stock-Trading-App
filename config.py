4bb96bedd3f69f7bf98c1fe5ab1a482707637b1b
Brian Franco <42521586+brianfranco141@users.noreply.github.com>
2021-11-30 22:25:00 -0800

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,29 @@
-#connection
-# #server startup:  uvicorn main:app --reload
-# API_KEY  = 'PK7RJV8636MP2HDRCDXK'
-# SECRET_KEY = 'l8gJ1OBJYIqjMSOXwsG9pw6nhUYx18mimLWeB7cW'
-# API_URL = 'https://paper-api.alpaca.markets'
-# DB_FILE = '/Users/brianfranco/Desktop/Developer/Projects/algorithmic-trading/fullstack-trading-app/app.db'
+# For testing purposes I would recommend creating and registering for an alpaca paper trading account thats very easy to make and best of all free.
+# Run this command to locally host server for the front layer and display
 
-# EMAIL_ADDRESS = 'jitsuri.trading.log@gmail.com'
-# EMAIL_SMS = '3103459404@messaging.sprintpcs.com'
-# EMAIL_PASSWORD = 'noVa69156289!'
-# EMAIL_HOST = 'smtp.gmail.com'
-# EMAIL_PORT = 465
-# #dawg water
+connection
+server startup:  uvicorn main:app --reload
+
+# Both the API_KEY and SECRET_KEY can be obtained through the alpaca trading platform 
+API_KEY  = 
+SECRET_KEY = 
+
+
+# have the option to change to real/paper trading environment.
+# paper tradin provides a way of testing with real world trading eviromnent as a way of back testing 
+# https://paper-api.alpaca.markets or 'https:api.alpaca.markets
+API_URL = 'https://paper-api.alpaca.markets'
+
+
+
+# Make sure for the DB_FILE to app.db us linked with the directory of your cloned project
+
+# EX: DB_FILE =  '/Users/brianfranco/Desktop/Developer/Projects/algorithmic-trading/Stock-Trading-App/app.db'
+DB_File = 
+
+# Can have the option to recieve tradding alerts when somethingis either sold or 
+EMAIL_ADDRESS = 
+EMAIL_SMS = 
+EMAIL_PASSWORD = 
+EMAIL_HOST = 
+EMAIL_PORT = 
