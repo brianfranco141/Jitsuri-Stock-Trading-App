887f19ce4e5ea708cc8389e3d70afe1578ab6e1b
Brian Franco <42521586+brianfranco141@users.noreply.github.com>
2021-11-30 22:24:38 -0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,13 @@
+#connection
+# #server startup:  uvicorn main:app --reload
+# API_KEY  = 'PK7RJV8636MP2HDRCDXK'
+# SECRET_KEY = 'l8gJ1OBJYIqjMSOXwsG9pw6nhUYx18mimLWeB7cW'
+# API_URL = 'https://paper-api.alpaca.markets'
+# DB_FILE = '/Users/brianfranco/Desktop/Developer/Projects/algorithmic-trading/fullstack-trading-app/app.db'
+
+# EMAIL_ADDRESS = 'jitsuri.trading.log@gmail.com'
+# EMAIL_SMS = '3103459404@messaging.sprintpcs.com'
+# EMAIL_PASSWORD = 'noVa69156289!'
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_PORT = 465
+# #dawg water
